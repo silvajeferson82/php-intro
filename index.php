44c1236fc0f15050d3fc4f853b7c6f1719129ad6
silvajeferson82 <jefersonsilva.contato@gmail.com>
2020-03-09 00:09:55 -0300

    Adicionado validaçao de campose exibiçao de mensagem de retorno

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+	session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,9 +12,20 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-	<p>FORMULARIO PARA INSCRIÇÃO DE COMPETIDIORES</p>
+	<p>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</p>
 
 	<FORM action="script.php" method="POST">
+		<?php
+			$mensagem_Sucesso = isset($_SESSION['mensagem de sucesso']) ? $_SESSION['mensagem de sucesso'] : '';
+			if(!empty($mensagem_Sucesso)){
+				echo $mensagem_Sucesso;
+			}
+
+			$mensagem_Erro = isset($_SESSION['mensagem de erro']) ? $_SESSION['mensagem de erro'] : '';
+			if(!empty($mensagem_Erro)){
+				echo $mensagem_Erro;
+			}
+		?>
 		<p>Seu Nome: <input type="text" name="nome"/></p>
 		<p>Sua Idade: <input type="text" name="idade"/></p>
 		<p><input type="submit" value="Enviar Dados" /></p>
--- script.php
+++ script.php
@@ -1,5 +1,7 @@
 <?php  
 
+session_start();
+
 $categoria = [];
 $categoria[] = "Infantil";
 $categoria[] = "Adolescente";
@@ -9,50 +11,62 @@ $categoria[] = "Master";
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-//var_dump($nome);
-//var_dump($idade);
-
 if(empty($nome)){
-	echo "O campo nome não pode ser vazio!";
+	$_SESSION['mensagem de erro'] = "Preencha os dados(nome e idade) para poder continuar!";
+	header('Location: index.php');
 	return;
 }
 
-if(strlen($nome) <= 3){
-	echo "O nome deve ter mais de 3 caracteres!";
+else if(strlen($nome) <= 3){
+	$_SESSION['mensagem de erro'] = "O nome deve ter mais de 3 caracteres!";
+	header('Location: index.php');
 	return;
 }
 
-if(strlen($nome) >= 20){
-	echo "Limite de até 20 caracteres execedido!";
+else if(strlen($nome) >= 20){
+	$_SESSION['mensagem de erro'] = "Limite de até 20 caracteres execedido!";
+	header('Location: index.php');
 	return;
 }
 
-if(!is_numeric($idade)){
-	echo "Informe um número para idade!";
+else if(!is_numeric($idade)){
+	$_SESSION['mensagem de erro'] = "Informe um número para idade!";
+	header('Location: index.php');
 	return;
 }
 
 if ($idade >= 6 && $idade <= 12) {
 	for ($i=0; $i < count($categoria); $i++) {
-		if($categoria[$i] == 'Infantil') 
-		echo "O Competidor ".$nome." disputa a categoria ".$categoria[$i]." !";
+		if($categoria[$i] == 'Infantil'){
+			$_SESSION['mensagem de sucesso'] = "O Competidor ".$nome." disputa a categoria ".$categoria[$i]." !";
+			header('Location: index.php');
+			return;
+		}
 	}
 }else if ($idade >= 13 && $idade <= 18) {
 	for ($i=0; $i < count($categoria); $i++) {
 		if($categoria[$i] == 'Adolescente') {
-		echo "O Competidor ".$nome." disputa a categoria ".$categoria[$i]." !";
+			$_SESSION['mensagem de sucesso'] = "O Competidor ".$nome." disputa a categoria ".$categoria[$i]." !";
+			header('Location: index.php');
+			return;
 		}
 	}
 }else if ($idade >= 19 && $idade <= 30) {
 	for ($i=0; $i < count($categoria); $i++) {
-		if($categoria[$i] == 'Senior') 
-		echo "O Competidor ".$nome." disputa a categoria ".$categoria[$i]." !";
+		if($categoria[$i] == 'Senior') {
+			$_SESSION['mensagem de sucesso'] = "O Competidor ".$nome." disputa a categoria ".$categoria[$i]." !";
+			header('Location: index.php');
+			return;
+		}
 	}
 }else{
 	
 	for ($i=0; $i < count($categoria); $i++) {
-		if($categoria[$i] == 'Master') 
-		echo "O Competidor ".$nome." disputa a categoria ".$categoria[$i]." !";
+		if($categoria[$i] == 'Master') {
+			$_SESSION['mensagem de sucesso'] = "O Competidor ".$nome." disputa a categoria ".$categoria[$i]." !";
+			header('Location: index.php');
+			return;
+		}
 	}
 }
 
